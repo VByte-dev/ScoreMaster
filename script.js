6ac8eee6cfbd7b1639694345f7d434ac9336d7d1
Vedhesh <vbyte.02@gmail.com>
2024-11-27 12:16:35 +0530

    Completed

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ document.querySelector("#clearB").addEventListener("click", () => {
 // Add Function
 let players = [];
 document.querySelector("#addB").addEventListener("click", () => {
-  let playerName = document.querySelector("#name").value;
-  let score = document.querySelector("#score").value;
-  let player = { name: playerName, score: score };
-  players.push(player);
-  document.querySelector("#name").value = "";
-  document.querySelector("#score").value = "";
+  try{
+    let playerName = document.querySelector("#name").value;
+    let score = document.querySelector("#score").value;
+    let player = { name: playerName, score: score };
+    players.push(player);
+    document.querySelector("#name").value = "";
+    document.querySelector("#score").value = "";
+  }catch(e){alert(e)}
 });
 // Generate Function
 document.querySelector("#genB").addEventListener("click", () => {
-  document.querySelector("#lb").innerHTML = "";
-  players.sort((a, b) => b.score - a.score);
-  players.forEach((player) => {
-    let html = `
-    <div id="lb1" class="flex justify-between px-8">
-      <h2>${player.name}</h2>
-      <h2>${player.score}</h2>
-    </div>`;
-    document.querySelector("#lb").innerHTML += html;
-  });
+  try {
+    doument.querySelector("#lb").innerHTML = "";
+    players.sort((a, b) => b.score - a.score);
+    players.forEach((player) => {
+      let html = `
+      <div id="lb1" class="flex justify-between px-8">
+        <h2>${player.name}</h2>
+        <h2>${player.score}</h2>
+      </div>`;
+      document.querySelector("#lb").innerHTML += html;
+    });
+  } catch (e) {
+    alert(e);
+  }
 });
